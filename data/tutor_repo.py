b6b400e565b0d901d87fa390e028bbba04e7abfb
BrendaaSouzaa <brendacontato06@gmail.com>
2025-06-25 14:59:03 -0300

    mexendo tutor e usuario

--- data/tutor_repo.py
+++ data/tutor_repo.py
@@ -1,7 +1,8 @@
-from typing import Optional, List
+from typing import Any, Optional
+from data import usuario_repo
 from data.tutor_model import Tutor
-from data.tutor_sql as tutor_sql
-from data.usuario_sql as usuario_sql
+from data.tutor_sql import *
+from data.usuario_model import Usuario
 from data.util import get_connection
 
   
@@ -11,36 +12,43 @@ def criar_tabela_tutor() -> bool:
         cursor.execute(CRIAR_TABELA)
         return cursor.rowcount > 0
 
+    
 def inserir_tutor(tutor: Tutor) -> Optional[int]:
     with get_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(usuario_sql.INSERIR, (
-            tutor.id_tutor,  
-            tutor.endereco))
-        return cursor.lastrowid
-        cursor.execute(usuario_sql.INSERIR, (
-            tutor.id_tutor,  
-            tutor.endereco
-            ))
-        return cursor.lastrowid
-
+        usuario = Usuario(0, 
+            tutor.nome, 
+            tutor.email, 
+            tutor.senha)
+        id_usuario = usuario_repo.inserir_usuario(usuario, cursor)
+        cursor.execute(INSERIR, (
+            id_usuario,
+            tutor.telefone))
+        return id_usuario
 
 def atualizar_tutor(tutor: Tutor) -> bool:
     with get_connection() as conn:
         cursor = conn.cursor()
         cursor.execute(ATUALIZAR, (
-            tutor.telefone, 
-            tutor.endereco, 
             tutor.id_tutor
             ))
         return cursor.rowcount > 0
-
-
-def excluir_tutor(id_tutor: int) -> bool:
+def atualizar_tutor(tutor: Tutor) -> bool:
     with get_connection() as conn:
         cursor = conn.cursor()
-        cursor.execute(EXCLUIR, (id_tutor,))
+        usuario = Usuario(tutor.id_usuario, 
+            tutor.nome, 
+            tutor.email, 
+            tutor.senha,
+            tutor.telefone)
+        usuario_repo.alterar_usuario(usuario, cursor)
+        cursor.execute(ATUALIZAR, (
+            tutor.id_usuario))
         return (cursor.rowcount > 0)
+    
+def excluir_tutor(id_tutor: int, cursor: Any) -> bool:
+    cursor.execute(EXCLUIR, (id_tutor,))
+    return (cursor.rowcount > 0)
 
 def obter_todos_tutores() -> list[Tutor]:
     with get_connection() as conn:
@@ -50,8 +58,7 @@ def obter_todos_tutores() -> list[Tutor]:
         tutores = [
             Tutor(
                 id_tutor=row["id_tutor"], 
-                telefone=row["telefone"], 
-                endereco=row["endereco"])
+                telefone=row["telefone"])
             for row in rows]
         return tutores
     
@@ -62,6 +69,5 @@ def obter_tutor_por_id(id_tutor: int) -> Optional[Tutor]:
         row = cursor.fetchone()
         tutor = Tutor(
                 id_tutor=row["id_tutor"], 
-                telefone=row["telefone"], 
-                endereco=row["endereco"])
+                telefone=row["telefone"])
         return tutor
--- data/tutor_sql.py
+++ data/tutor_sql.py
@@ -1,24 +1,26 @@
 CRIAR_TABELA = """
 CREATE TABLE IF NOT EXISTS tutor (
     id_tutor INTEGER PRIMARY KEY,
+    
     FOREIGN KEY (id_tutor) REFERENCES usuario(id_usuario)
 );
 """
-INSERIR = """
-INSERT INTO tutor (telefone, endereco)
-VALUES (?, ?);
-"""
+# INSERIR = """
+# INSERT INTO tutor (telefone, endereco)
+# VALUES (?, ?);
+# """
 
-ATUALIZAR = """
-UPDATE tutor
-SET telefone = ?, endereco = ?
-WHERE id_tutor = ?;
-"""
+# ATUALIZAR = """
+# UPDATE tutor
+# SET telefone = ?, endereco = ?
+# WHERE id_tutor = ?;
+# """
 
 EXCLUIR = """
 DELETE FROM tutor 
 WHERE id_tutor = ?;
 """
+
 OBTER_TODOS = """
 SELECT 
 t.id_tutor,
@@ -29,6 +31,7 @@ FROM tutor t
 INNER JOIN usuario u ON t.id_tutor = u.id_usuario
 ORDER BY u.nome = ?;
 """
+
 OBTER_POR_ID = """
 SELECT 
 t.id_tutor,
@@ -36,6 +39,6 @@ u.nome,
 u.email,
 u.telefone
 FROM tutor t
-JOIN usuario u ON t.id_usuario = u.id_usuario
+INNER JOIN usuario u ON t.id_usuario = u.id_usuario
 WHERE t.id_tutor = ?;
 """
